81358fedc47dafe05a39fad3d9bbb10988aca6c4
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-06-27 22:14:52 -0400

    added vscode + day 1 + run.exe

---
+++ 06222022.cpp
@@ -0,0 +1,14 @@
+/*
+    Day 1 of C++
+    - Hello World :)
+*/
+
+#include <iostream>
+// the io (input / output) header thingy
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
