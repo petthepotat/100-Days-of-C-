7bf38f9adeedd9ab08c35642a4ffe7eb60b2a1cd
Peter Zhang <ultraswordkirbypink@gmail.com>
2022-07-02 12:09:03 -0400

    added day 7

--- 100 days of C++/7.cpp
+++ 100 days of C++/7.cpp
@@ -1,12 +1,38 @@
 /*
-    Day 7
-    - Hello world in c++
+    Day 8 of C++
+    - file reader :)
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+
+std::string filename;
+std::ifstream file;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    // get filepath
+    std::cout << "Input the file path relative to this program~ \n>";
+    getline(std::cin, filename);
+
+    // open file
+    file.open(filename);
+
+    // check if not opened
+    if (!file || !file.is_open())
+    {
+        std::cout << "Failed to open file at |" << filename << "|" << std::endl;
+        file.close();
+        return 0;
+    }
+
+    std::string line;
+    int i = 0;
+    while ((bool)getline(file, line))
+        std::cout << "[Line " << i++ << "] " << line << std::endl;
+
+    file.close();
+
     return 0;
 }
